7f7dca8b7679f3f8b0d9148223269a2b34e969f9
Ernani César <ernani.ecs@gmail.com>
2018-12-24 22:39:03 -0200

    Restore master branch to remove glue code implementation.

--- src/test/java/br/ufsc/concordion/fixtures/WithdrawFixture.java
+++ src/test/java/br/ufsc/concordion/fixtures/WithdrawFixture.java
@@ -1,59 +1,11 @@
 package br.ufsc.concordion.fixtures;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
-import br.ufsc.model.ATM;
-import br.ufsc.model.PinHistory;
 
 @RunWith(ConcordionRunner.class)
 public class WithdrawFixture {
-	/*public String customerName;
-	public String accountNumber;
-	public double accountBalance;
-	public String bankCardNumber;
-	public int bankCardPIN;*/
 	
-	public ATM atm;
-	//public int pin;
-	//public int amount;
-	
-	public void setCustomer(String customerName, String accountNumber, double accountBalance, String bankCardNumber, int bankCardPIN) {
-		atm = new ATM();
-		atm.getBank().createBankCustomer(1, customerName, bankCardNumber, bankCardPIN, accountNumber, accountBalance);
-	}
-	
-	public void setAvailableBills(String bills) {
-		String[] arrayBills = bills.split(",");
-		atm.chargeBills(Integer.parseInt(arrayBills[0]), Integer.parseInt(arrayBills[1]), Integer.parseInt(arrayBills[2]), Integer.parseInt(arrayBills[3]), Integer.parseInt(arrayBills[4]));
-	}
-	
-	public String withdraw(String bankCardNumber, int pin, int amount) {
-		try {
-			atm.withDraw(bankCardNumber, pin, amount);
-			return "A transação foi completada com sucesso!";
-		} catch (Exception e) {
-			return e.getMessage();
-		}
-	}
-	
-	public double getBalance(String cardNumber) throws Exception {
-		try {
-			return atm.getBank().getAccountByCardNumber(cardNumber).getBalance();
-		} catch (Exception e) {
-			throw new Exception(e.getMessage());
-		}
-	}
-	
-	public void setNumberOfIncorretPINWithin72Hours(String text, String bankCardNumber) throws Exception {
-		atm.getBank().getCardByNumber(bankCardNumber).setPinHistory(new ArrayList<PinHistory>());
-		int x = Integer.parseInt(text);
-		for (int i=1; i<=x; i++) {
-			atm.getBank().getCardByNumber(bankCardNumber).insertPinHistory(false, new Date());
-		}
-	}
 }
