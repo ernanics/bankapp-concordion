90187b92ed6c39b7acc281bb8dd1fbb2ff6fa852
Hallan Medeiros <hallan.medeiros@tjsc.jus.br>
2019-02-01 19:34:54 -0200

    tentativas

--- src/test/java/br/ufsc/concordion/fixtures/EMICalculatorFixture.java
+++ src/test/java/br/ufsc/concordion/fixtures/EMICalculatorFixture.java
@@ -3,12 +3,41 @@ package br.ufsc.concordion.fixtures;
 import java.util.HashMap;
 import java.util.Map;
 
->>>>>>> refs/remotes/origin/ConcordionEx001
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
+import br.ufsc.model.ATM;
+import br.ufsc.model.Loan;
+
 @RunWith(ConcordionRunner.class)
 public class EMICalculatorFixture {
-
 	
+	public Loan loan;
+	public ATM atm;
+
+	//before
+	public void setCustomer(String customerName, String accountNumber, double accountBalance, String bankCardNumber, int bankCardPIN) {
+		atm = new ATM();
+		atm.getBank().createBankCustomer(1, customerName, bankCardNumber, bankCardPIN, accountNumber, accountBalance);
+	}
+
+	//set available bills
+	public void setAvailableBills(String bills) {
+		String[] arrayBills = bills.split(",");
+		atm.chargeBills(Integer.parseInt(arrayBills[0]), Integer.parseInt(arrayBills[1]), Integer.parseInt(arrayBills[2]), Integer.parseInt(arrayBills[3]), Integer.parseInt(arrayBills[4]));
+	}
+	
+	public Map<String, String> calculateEMI(Double amount, Double interestRate, Long numberOfMonthlyInstallments) {
+		loan = new Loan(amount, interestRate, numberOfMonthlyInstallments);
+		loan.getNumberOfMontlhyInstalments();
+		loan.getLoanAmountWithInterests();
+		Map<String, String> results = new HashMap<String, String>();
+	    results.put("emiValue", loan.getValueOfMontlhyInstalments().toString());
+	    results.put("loanAmountWithInterest", loan.getLoanAmountWithInterests().toString());
+	    return results;
+	}
+	
+//	public void calculateEMI(String amout, String interestRate, String numberOfMonthlyInstallments) {
+//		
+//	}
 }
--- src/test/java/br/ufsc/concordion/fixtures/WithdrawFixture.java
+++ src/test/java/br/ufsc/concordion/fixtures/WithdrawFixture.java
@@ -3,16 +3,44 @@ package br.ufsc.concordion.fixtures;
 
 import java.util.ArrayList;
 import java.util.Date;
->>>>>>> refs/remotes/origin/ConcordionEx001
+import java.util.Map;
 
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
+
+import br.ufsc.model.ATM;
 
 
 @RunWith(ConcordionRunner.class)
 public class WithdrawFixture {
 	
+	public ATM atm;
 
+	public void setCustomer( String customerName, String accountNumber, double accountBalance, String bankCardNumber, int bankCardPIN) {
+		atm = new ATM();
+		atm.getBank().createBankCustomer(1, customerName, bankCardNumber, bankCardPIN, accountNumber, accountBalance);
+	}
+	
+	public void setAvailableBills(String bills) {
+		String[] arrayBills = bills.split(",");
+		atm.chargeBills(Integer.parseInt(arrayBills[0]), Integer.parseInt(arrayBills[1]), Integer.parseInt(arrayBills[2]), Integer.parseInt(arrayBills[3]), Integer.parseInt(arrayBills[4]));
+	}
 
-
+	public String withdraw(String bankCardNumber, int pin, int amount) throws Exception {
+		try {
+			Map<Integer, Integer> wd = atm.withDraw(bankCardNumber, pin, amount);
+			return "A transação foi completada com sucesso!";
+		}
+		catch(Exception ex) {
+			return ex.getMessage();
+		}
+	}
+	
+	public double getBalance(String bankCardNumber) throws Exception {
+		return atm.getBank().getAccountByCardNumber(bankCardNumber).getBalance();
+	}
+	
+	public void setNumberOfIncorretPINWithin72Hours(String TEXT, String bankCardNumber) {
+//		atm.getBank().getCardByNumber(bankCardNumber).
+	}
 }
